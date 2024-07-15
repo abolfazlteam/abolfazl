5160770c99011a333b14f04034199382d6a3a7c0
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-07-15 10:20:58 +0330

    update navlink component and add useOutsideClick hook

--- src/components/Navigation/NavLink.tsx
+++ src/components/Navigation/NavLink.tsx
@@ -14,7 +14,7 @@ const NavLink: React.FC<INavLinkProps> = ({ children, href }) => {
   return (
     <Link
       href={href}
-      className={`relative cursor-pointer text-caption1 transition-all duration-75 ease-linear ${isLinkActive ? "before:shadow-navActiveLink before:bg-gradient-nav-link font-medium text-primary before:absolute before:top-full before:mt-2 before:inline-block before:h-[2px] before:w-full" : "text-gray-1 hover:text-primary"} `}
+      className={`relative cursor-pointer text-caption1 transition-all duration-75 ease-linear ${isLinkActive ? "font-medium text-primary before:absolute before:top-full before:mt-2 before:inline-block before:h-[2px] before:w-full before:bg-gradient-nav-link before:shadow-navActiveLink" : "text-gray-1 hover:text-primary"} `}
     >
       {children}
     </Link>
