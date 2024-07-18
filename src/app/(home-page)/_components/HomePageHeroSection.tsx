a99e6824f32c9dd45c03bde5028371d939992d43
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-07-18 08:37:41 +0330

    update home page hero section titles' font size

--- src/app/(home-page)/_components/HomePageHeroSection.tsx
+++ src/app/(home-page)/_components/HomePageHeroSection.tsx
@@ -13,10 +13,10 @@ const HomePageHeroSection = () => {
     <section className="flex h-full max-h-[650px] flex-col items-center gap-6 sm:mt-20 sm:max-h-[380px] sm:flex-row md:gap-8">
       <article className="flex w-full flex-col gap-4 md:gap-8">
         <header className={`${rammettoOne.className}`}>
-          <h1 className="text-xl leading-8 lg:text-[1.9rem] lg:leading-[3.4rem]">
+          <h1 className="text-xl leading-8 md:text-[1.6rem] lg:text-[1.9rem] lg:leading-[3.4rem]">
             Abolfazl Jamshidi
           </h1>
-          <p className="text-lg leading-8 text-primary lg:text-[1.8rem] lg:leading-[3rem]">
+          <p className="text-lg leading-8 text-primary md:text-[1.5rem] lg:text-[1.8rem] lg:leading-[3rem]">
             Front-end developer
           </p>
         </header>
