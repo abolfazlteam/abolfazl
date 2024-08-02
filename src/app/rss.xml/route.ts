954f1de23bfc229c7a2cd9d12b2fc909850026f0
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-08-02 09:40:56 +0330

    add rss.xml to generate feed

--- src/app/rss.xml/route.ts
+++ src/app/rss.xml/route.ts
@@ -1,39 +1,39 @@
-// import RSS from "rss";
+import RSS from "rss";
 // import { allArticles } from "contentlayer/generated";
 
 export async function GET() {
   //   // creating an RSS instance
-  //   const feed = new RSS({
-  //     title: "Arman Ahmadi - Backend Engineer | Articles RSS Feed",
-  //     description: `Hey there! This is where I share my journey through software engineering, personal anecdotes, and articles covering a wide range of topics.`,
-  //     site_url: "https://armancodes.com",
-  //     feed_url: "https://armancodes.com/rss.xml",
-  //     image_url: `https://armancodes.com/images/white-logo.jpg`,
-  //     managingEditor: "me@armancodes.com (Arman Ahmadi)",
-  //     webMaster: "me@armancodes.com (Arman Ahmadi)",
-  //     generator: "RSS for Node and Next.js",
-  //     copyright: `Copyright ${new Date().getFullYear().toString()}, Arman Ahmadi`,
-  //     language: "en-US",
-  //     pubDate: new Date().toUTCString(),
-  //     ttl: 60,
-  //   });
-  //   if (allArticles) {
-  //     allArticles
-  //       .filter((article) => !article.isDraft)
-  //       .map((article) => {
-  //         feed.item({
-  //           title: article.title,
-  //           description: article.summary,
-  //           url: `${article.baseUrl}/articles/${article.slug}`,
-  //           categories: article.tags || [],
-  //           author: article.author,
-  //           date: article.publishedAt,
-  //         });
+  const feed = new RSS({
+    title: "Abolfazl Jamshidi - Frontend Engineer | Blogs RSS Feed",
+    description: `Hey there! This is where I share my journey through software engineering, personal anecdotes, and articles covering a wide range of topics.`,
+    site_url: "https://iabolfazl.dev",
+    feed_url: "https://iabolfazl.dev/rss.xml",
+    image_url: `https://iabolfazl.dev/images/white-logo.jpg`,
+    managingEditor: "abolfazljamshididev@gmail.com (Abolfazl Jamshidi)",
+    webMaster: "abolfazljamshididev@gmail.com (Abolfazl Jamshidi)",
+    generator: "RSS for Node and Next.js",
+    copyright: `Copyright ${new Date().getFullYear().toString()}, Abolfazl Jamshidi`,
+    language: "en-US",
+    pubDate: new Date().toUTCString(),
+    ttl: 60,
+  });
+  // if (allArticles) {
+  //   allArticles
+  //     .filter((article) => !article.isDraft)
+  //     .map((article) => {
+  //       feed.item({
+  //         title: article.title,
+  //         description: article.summary,
+  //         url: `${article.baseUrl}/articles/${article.slug}`,
+  //         categories: article.tags || [],
+  //         author: article.author,
+  //         date: article.publishedAt,
   //       });
-  //   }
-  //   return new Response(feed.xml({ indent: true }), {
-  //     headers: {
-  //       "Content-Type": "application/xml; charset=utf-8",
-  //     },
-  //   });
+  //     });
+  // }
+  return new Response(feed.xml({ indent: true }), {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8",
+    },
+  });
 }
