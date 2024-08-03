260870ab2aba51bf755efbd24853d8f614aec428
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-08-03 07:40:15 +0330

    add sample project content (#59)

--- src/app/(projects)/projects/[slug]/page.tsx
+++ src/app/(projects)/projects/[slug]/page.tsx
@@ -1,6 +1,9 @@
+// import type { Project as ProjectType } from "contentlayer/generated";
+import { allProjects } from "contentlayer/generated";
+
 import BackLink from "@/components/ui/BackLink";
 import ProjectHeader from "./_components/ProjectHeader";
-// import MdxWrapper from "@/app/(blogs)/blogs/[slug]/_components/mdx/MdxWrapper";
+import MdxWrapper from "@/app/(blogs)/blogs/[slug]/_components/mdx/MdxWrapper";
 import TagsList from "@/app/(blogs)/blogs/[slug]/_components/TagsList";
 import Newsletter from "@/components/ui/Newsletter";
 
@@ -20,14 +23,13 @@ const Page = ({ params }: Props) => {
 
       {/* BODY SECTION */}
       <section className="sm:space-x-6 md:space-x-14">
-        {/* <MdxWrapper code={blog?.body?.code as string} /> */}
-
-        {/* TAGS SECTION */}
-        <TagsList tags={["react", "next.js"]} />
-
-        {/* Newsletter */}
-        <Newsletter />
+        <MdxWrapper code={allProjects[0]?.body?.code as string} />
       </section>
+      {/* TAGS SECTION */}
+      <TagsList tags={["react", "next.js"]} />
+
+      {/* Newsletter */}
+      <Newsletter />
     </main>
   );
 };
