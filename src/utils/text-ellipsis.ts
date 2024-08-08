e0efa204435ea344ec9f4026d885f21238f97cdc
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2024-08-08 07:18:25 +0330

    add tests for text truncate helper function

--- src/utils/text-ellipsis.ts
+++ src/utils/text-ellipsis.ts
@@ -1,18 +1,31 @@
 /**
- * @param {string} text
- * @param {number} count
- * @returns {string} returns the formatted text string with "..." ellipsis based on the text length and count value for characters
+ * Formats the given text to a specified length with an ellipsis if it exceeds that length.
+ * @param {string} text - The text to format.
+ * @param {number} count - The maximum number of characters to keep.
+ * @returns {string} The formatted text string with "..." if truncated.
  */
 export default function textEllipsisFormatter(
   text: string,
   count: number,
 ): string {
-  const textLength = text.split("").length;
-  const newText = text.split("").splice(0, count).join("");
-
-  if (textLength > count) {
-    return `${newText}` + "...";
-  } else {
-    return newText;
+  // type guard
+  if (typeof text !== "string" || typeof count !== "number") {
+    throw new Error("Invalid input type.");
   }
+
+  // count less than 0
+  if (count < 0) {
+    throw new Error("Count must be greater than zero!");
+  }
+
+  // return the complete text if the count is 0
+  if (count === 0) {
+    return text;
+  }
+
+  if (text.length <= count) {
+    return text;
+  }
+
+  return text.slice(0, count) + "...";
 }
