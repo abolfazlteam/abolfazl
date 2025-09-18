bbf7f0dff5f7c4e8364dca69e8df7a5045106952
Abolfazl Jamshidi <abolfazljamshididev@gmail.com>
2025-09-18 21:37:52 +0330

    docs: add astrobloom theme document to project

--- src/app/(projects)/_components/ProjectsList.tsx
+++ src/app/(projects)/_components/ProjectsList.tsx
@@ -3,12 +3,17 @@ import ProjectCard from "./ProjectCard";
 
 const ProjectsList = () => {
   const publishedProjects = PROJECTS_DATA.filter((project) => !project.isDraft);
-  const firstThreeProjects = publishedProjects.slice(0, 2);
+  const firstThreeProjects = publishedProjects.slice(0, 3);
+  const astrobloomTheme = publishedProjects?.find(
+    (item) => item?.slug === "astrobloom-theme",
+  );
+
+  const finalList = [astrobloomTheme!, ...firstThreeProjects];
 
   return (
     <div className="flex flex-wrap items-center gap-6">
-      {firstThreeProjects?.map((project) => (
-        <ProjectCard key={project.id} {...project} width={372} />
+      {finalList?.map((project) => (
+        <ProjectCard key={project?.id} {...project} width={372} />
       ))}
     </div>
   );
